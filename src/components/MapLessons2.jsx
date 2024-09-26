c2d060d57c392cab15f0e6ccebd44c382767e84b
Sebbie Mzing <Sebbie Evayo>
2024-09-26 16:53:21 +0300

    optimize background image

--- src/components/MapLessons2.jsx
+++ src/components/MapLessons2.jsx
@@ -4,10 +4,13 @@ function MapLessons2() {
   return (
     <div>
         <h1 className='font-bold text-2xl'>Map Lessons</h1>
-        <h3 className='bg-slate-100 p-4'>Application in School</h3>
+        <h3 className='bg-slate-100 p-4 font-bold'>Application in School</h3>
         <p className='bg-slate-100 p-4'>Research-based projects</p>
         <p className='bg-slate-100 p-4'>Ethical considerations in technology</p>
-        <h3 className='bg-slate-100 p-4'>Application in career preparation</h3>
+        <h3 className='bg-slate-100 p-4 font-bold'>Application in career preparation</h3>
+        <p className='bg-slate-100 p-4'>Problem-solving skills</p>
+        <p className='bg-slate-100 p-4'>Human-centered design approach</p>
+
     </div>
   )
 }
